0a840a4839881d65a1a596553fafd8fb324d105f
jball6700 <85566303+jball6700@users.noreply.github.com>
2021-06-08 14:33:27 -0400

    creating series and dataframe

--- pandas_intro.py
+++ pandas_intro.py
@@ -1,6 +1,14 @@
+#Imports
 import numpy as np
 import pandas as pd
 
+#Object Creation
 s = pd.Series([1,3,5,np.nan,6,8])
-
 print(s)
+
+#Create a DataFrame
+dates = pd.date_range("20130101", periods=6)
+print(dates)
+
+df = pd.DataFrame(np.random.randn(6,4), index= dates, columns-list("ABCD"))
+print(df)
