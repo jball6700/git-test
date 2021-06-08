a0fda45f3e2426e0c3339ea4a664546db862a3dc
jball6700 <85566303+jball6700@users.noreply.github.com>
2021-06-08 14:34:30 -0400

    spell error

--- pandas_intro.py
+++ pandas_intro.py
@@ -10,5 +10,5 @@ print(s)
 dates = pd.date_range("20130101", periods=6)
 print(dates)
 
-df = pd.DataFrame(np.random.randn(6,4), index= dates, columns-list("ABCD"))
+df = pd.DataFrame(np.random.randn(6,4), index= dates, columns=list("ABCD"))
 print(df)
