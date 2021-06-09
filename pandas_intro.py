b670afa0e8c61b41d43efe1465a69989a020b131
jball6700 <85566303+jball6700@users.noreply.github.com>
2021-06-09 15:34:50 -0400

    Plotting and CSV

--- pandas_intro.py
+++ pandas_intro.py
@@ -333,8 +333,27 @@ print(df.groupby("grade").size())
 #Plotting
 
 #use the standard convention for referencing the matplotlib API:
-  #import matplotlib.pyplot as plt
-  #plt.close("all")
-  #ts = pd.Series(np.random.randn(1000), index=pd.date_range("1/1/2000", periods=1000))
-  #ts = ts.cumsum()
-  #ts.plot()
+import matplotlib.pyplot as plt
+plt.close("all")
+ts = pd.Series(np.random.randn(1000), index=pd.date_range("1/1/2000", periods=1000))
+ts = ts.cumsum()
+print(ts.plot())
+
+#On a DataFrame, the plot() method is a convenience to plot all of the columns with labels:
+df = pd.DataFrame(
+        np.random.randn(1000, 4), index=ts.index, columns=["A","B","C","D"]
+    )
+df = df.cumsum()
+print(plt.figure())
+print(df.plot())
+
+
+
+#Getting data in/out
+
+#CSV
+#Writing to a csv file
+df.to_csv("foo.csv")
+
+#reading from a csv file
+print(pd.read_csv("foo.csv"))
